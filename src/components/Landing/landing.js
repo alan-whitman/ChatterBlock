9f47cc210b11324d6df1295b42aed7e474f43bd1
Isaac Worthen <pinecone@Isaacs-MacBook-Pro.local>
2019-01-08 11:47:53 -0700

    built in Routing and started a landing component

---
+++ src/components/Landing/landing.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+
+class Landing extends Component {
+    render(){
+        return (
+            <div>
+                
+                {/*Authentication turnary for landing*/}
+                {/* {isAuthenticated ? <Redirect to="/dashboard" /> : <div>
+
+                </div>} */}
+            </div>
+        )
+    }
+}
+
+export default Landing;
