0639f14784daeaf47a25b5e8ac218ae37f7b6109
Alan Whitman <alanwhitman>
2019-01-09 15:48:43 -0700

    App.js puts session user in store if it exists on the server

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import Landing from './components/Landing/landing';
 import Dashboard from './components/Dashboard/dashboard';
+import { connect } from 'react-redux';
+import { userLoggedIn } from './redux/reducer';
 import './reset.css';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  componentDidMount() {
+    axios.get('/auth/currentUser').then(res => {
+      if (res.data)
+        this.props.userLoggedIn(res.data);
+    })
+  }
   render() {
     return (
       <HashRouter>
@@ -24,4 +33,12 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  let { user, isAuthenticated } = state;
+  return {
+    user,
+    isAuthenticated
+  }
+}
+
+export default connect(mapStateToProps, {userLoggedIn})(App);
