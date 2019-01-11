33e7dd599e827055dca13f9ad480f4846317dd9c
Isaac Worthen <pinecone@Isaacs-MacBook-Pro.local>
2019-01-11 09:12:22 -0700

    messing with right-click popups

--- src/components/Dashboard/FriendsBar_ChannelUsers/frienduserbar.js
+++ src/components/Dashboard/FriendsBar_ChannelUsers/frienduserbar.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import './frienduserbar.css'
 import friends from './friendsDummyData';
+import { Redirect } from 'react-router-dom';
+import Popup from 'reactjs-popup';
 
 class FriendUserBar extends Component {
     constructor() {
         super();
         this.state = {
-            friends: friends
+            friends: friends,
+            rightclickmenu: false
         }
     }
     componentDidMount() {
         
     }
+
+    handleClick = (event) => {
+        if (event.type === 'click') {
+            // <Redirect to="/dashboard/profile"/>;
+        } else if (event.type === 'contextmenu') {
+            event.preventDefault();
+            
+            this.setState({ rightclickmenu: true }, () => {
+                document.addEventListener('click', this.closeMenu);
+            });
+        }
+    }
+
+    closeMenu = () => {
+        this.setState({ rightclickmenu: false }, () => {
+          document.removeEventListener('click', this.closeMenu);
+        });
+    }
+
     renderFriends() {
         let onlineFriends = this.state.friends
             .filter(friend => friend.online)
@@ -20,7 +42,7 @@ class FriendUserBar extends Component {
         let offlineFriends = this.state.friends
             .filter(friend => !friend.online)
             .sort((a, b) => a.username < b.username ? -1 : 1)
-            .map((friend, i) => <li key={i}>{friend.username}</li>);
+            .map((friend, i) => <Popup trigger={<li key={i} onClick={this.handleClick} onContextMenu={this.handleClick}>{friend.username}</li>} position="top left"><div>Popup content here !!</div></Popup>);
         return (
             <div>
                 <div>Online</div>
@@ -32,6 +54,7 @@ class FriendUserBar extends Component {
                     {offlineFriends}
                 </ul>
             </div>
+            
         )
     }
     render(){
@@ -40,6 +63,8 @@ class FriendUserBar extends Component {
                 <div className="friends-holder">
                     {this.renderFriends()}
                 </div>
+                {this.state.rightclickmenu && <div>hello</div>}
+                <Popup open={this.state.rightclickmenu} ><div>Popup content here !!</div></Popup>
             </div>
         )
     }
