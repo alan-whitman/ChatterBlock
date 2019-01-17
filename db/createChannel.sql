b9cf21b184a41ade2c2b3793a142945dd87f7f13
Nicholas Labrum <nicholaslabrum@Nicholass-MacBook-Pro.local>
2019-01-17 11:38:12 -0700

    description functionality

--- db/createChannel.sql
+++ db/createChannel.sql
@@ -1,3 +1,3 @@
-INSERT INTO channel (channel_name, creator_id)
-VALUES (${channel_name}, ${creator_id})
+INSERT INTO channel (channel_name, creator_id, channel_description)
+VALUES (${channel_name}, ${creator_id}, ${channel_description})
 RETURNING *;
