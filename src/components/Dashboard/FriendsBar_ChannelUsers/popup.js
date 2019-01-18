a0af228c0335e96feec27124d2ec4d4c11148ac6
Isaac Worthen <pinecone@Isaacs-MacBook-Pro.local>
2019-01-18 10:18:44 -0700

    styled friend requests

--- src/components/Dashboard/FriendsBar_ChannelUsers/popup.js
+++ src/components/Dashboard/FriendsBar_ChannelUsers/popup.js
@@ -38,12 +38,12 @@ class FriendsPopup extends Component {
     render(){
         const { friend } = this.props;
         return (
-            <div style={{position: 'relative', height: '30px', marginLeft: '15px'}}>
+            <div style={{height: '30px', marginLeft: '15px'}}>
                 <h3 onMouseDown={e=>this.handleMouse(e)} onContextMenu={this.handleClick}>{friend.username}</h3><br />
                 {this.state.show && <div className="popupmenu" style={{top: `calc(${this.state.y}px - 130px)`, left: `calc(${this.state.x}px - 200px`}}>
                     <div className="popup-sections" onClick={() => {this.props.history.push(`/dashboard/profile/${friend.id}`)}}>Profile</div>
                     <div className="popup-sections" onClick={() => {this.props.history.push(`/dashboard/dm/${friend.username}`)}}>Send Message</div>
-                    <span onClick={e => this.deleteFriend({id: friend.id, username: friend.username})} className="accept-reject">Remove Friend</span>
+                    <span onClick={e => this.deleteFriend({id: friend.id, username: friend.username})} className="delete">Remove Friend</span>
 
                 </div>}
             </div>
