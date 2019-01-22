5bb2bfb7507e7698d91cd210c201a5ea5d57b0fe
Brian Jones <BriansMac@Brians-MacBook-Pro.local>
2019-01-22 16:43:50 -0700

    reactions are working.  still need to bring in usernames on hover and fix off click for reaction options

--- src/components/Dashboard/ChannelView/ChannelViewMessage.js
+++ src/components/Dashboard/ChannelView/ChannelViewMessage.js
@@ -3,7 +3,133 @@ import { Link } from 'react-router-dom';
 import getDate from '../../DateFormat/dateStamp';
 
 class Message extends React.PureComponent {
+    constructor(){
+        super()
+        this.state={
+            reactionOptions:[
+                {value:'thumbs-up',
+                class: 'fas fa-thumbs-up'
+                },
+                {value: 'thumbs-down',
+                class: 'fas fa-thumbs-down'
+                },
+                {value: 'tired',
+                class: 'fas fa-tired'
+                },
+                {value: 'surprise',
+                class: 'fas fa-surprise'
+                },
+                {value: 'smile-wink',
+                class: 'fas fa-smile-wink'
+                },
+                {value: 'smile-beam',
+                class: 'fas fa-smile-beam'
+                },
+                {value: 'sad-tear',
+                class: 'fas fa-sad-tear'
+                },
+                {value: 'sad-cry',
+                class: 'fas fa-sad-cry'
+                },
+                {value: 'meh-rolling-eyes',
+                class: 'fas fa-meh-rolling-eyes'
+                },
+                {value: 'meh',
+                class: 'fas fa-meh'
+                },
+                {value: 'grin-wink',
+                class: 'fas fa-grin-wink'
+                },
+                {value: 'grin-squint-tears',
+                class: 'fas fa-grin-squint-tears'
+                },
+                {value: 'grin-hearts',
+                class: 'fas fa-grin-hearts'
+                },
+                {value: 'grin-beam',
+                class: 'fas fa-grin-beam'
+                },
+                {value: 'grin-alt',
+                class: 'fas fa-grin-alt'
+                },
+                {value: 'grin',
+                class: 'fas fa-grin'
+                },
+                {value: 'laugh-wink',
+                class: 'fas fa-laugh-wink'
+                },
+                {value: 'laugh-squint',
+                class: 'fas fa-laugh-squint'
+                },
+                {value: 'laugh-beam',
+                class: 'fas fa-laugh-beam'
+                },
+                {value: 'kiss-wink-heart',
+                class: 'fas fa-kiss-wink-heart'
+                },
+                {value: 'kiss-beam',
+                class: 'fas fa-kiss-beam'
+                },
+                {value: 'kiss',
+                class: 'fas fa-kiss'
+                },
+                {value: 'kiss-beam',
+                class: 'fas fa-kiss-beam'
+                },
+                {value: 'grin-tongue-squint',
+                class: 'fas fa-grin-tongue-squint'
+                },
+                {value: 'grin-tongue',
+                class: 'fas fa-grin-tongue'
+                },
+                {value: 'frown-open',
+                class: 'fas fa-frown-open'
+                },
+                {value: 'frown',
+                class: 'fas fa-frown'
+                },
+                {value: 'flushed',
+                class: 'fas fa-flushed'
+                },
+                {value: 'dizzy',
+                class: 'fas fa-dizzy'
+                }
+            ],
+            displayReactions: false
+        }
+
+    }
+
+    showReactionOptions = () => {
+        this.setState({
+            displayReactions: !this.state.displayReactions
+        })
+    }
+
     render() {
+        let reactionOptions = this.state.reactionOptions.map((reaction,i) =>{
+            return <div className="message-reaction-option" key={i}>
+            <span>
+                <i className={reaction.class} value={reaction.value} onClick={() => {this.props.likeMessage(this.props.message.id, reaction.value);this.showReactionOptions()}}>
+                </i>
+                </span>
+            </div>
+        })
+
+        let reactions = []
+        for (var key in this.props.message.reactions){
+            reactions.push([key])    
+        }
+        // console.log(reactions)
+        let userReactions = reactions.map((reaction,i) =>{
+            return <div key={i} className="message-reactions">
+                <span>
+                    <i className={`fas fa-${reaction}`} value={reaction} onClick={() => this.props.likeMessage(this.props.message.id, reaction[0])}></i>{this.props.message.reactions[reaction].length > 1 ? this.props.message.reactions[reaction].length:null}
+                </span>
+                {/* {console.log(this.props.message.reactions.getOwnPropertyNames,this.props.message.reactions[reaction].length)} */}
+            </div>
+        })
+        
         return (
             <div className="user-message" ref={this.props.messageRef}>
                 <div className="message-data">
@@ -11,10 +137,23 @@ class Message extends React.PureComponent {
                     <div className="time-stamp">{getDate(this.props.message.time_stamp)}</div>
                 </div>
                 <div className="message-content">{this.props.message.content_text}</div>
-                <div className="message-reactions">
-                    <span>
-                        <i className="fas fa-thumbs-up" onClick={() => this.props.likeMessage(this.props.message.id)}></i> {this.props.likes > 0 ? this.props.likes : null}
-                    </span>
+                <div className="toggleReactions" id="toggleReactions">
+                <i className="fas fa-smile" onClick={this.showReactionOptions}/>
+                </div>
+                <div className="reaction-options">
+                    {this.state.displayReactions === true ?
+                    <div id="someId">
+                        <div onClick={(e) => e.stopPropagation()}>
+                        {reactionOptions}
+                        </div>
+                    </div>
+                :
+                null
+                }
+                </div>
+               
+                <div className="userReactions">
+                {userReactions}
                 </div>
             </div>
         )
