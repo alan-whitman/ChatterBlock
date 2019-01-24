7fa0b2418b935f97df6c1ba05dd6a414eb31b0de
Alan Whitman <alanwhitman>
2019-01-24 09:55:48 -0700

    pin and close functionality on alerts in

--- src/components/Dashboard/Recent_Notifications/recent.js
+++ src/components/Dashboard/Recent_Notifications/recent.js
@@ -4,27 +4,26 @@ import './recent.css';
 import { createAlertMessage } from '../../../redux/reducer';
 
 class Recent extends Component {
-    constructor() {
-        super();
-        this.divRef = React.createRef();
-    }
-    state = {
-        showDiv: 'none'
-    }
-    show() {
-        this.setState({showDiv: 'block'}, () => this.divRef.current.focus());
+    // constructor() {
+    //     super();
+    //     this.divRef = React.createRef();
+    // }
+    // state = {
+    //     showDiv: 'none'
+    // }
+    // show() {
+    //     this.setState({showDiv: 'block'}, () => this.divRef.current.focus());
         
-    }
-    hide() {
-        this.setState({showDiv: 'none'});
-    }
+    // }
+    // hide() {
+    //     this.setState({showDiv: 'none'});
+    // }
     render(){
-        console.log(this.state.showDiv);
         return (
             <div className="Recent">
                 <h1 style={{color: 'white'}}>Recent stuff</h1>
-                <button onClick={e => this.show()}>click</button>
-                <div tabIndex="1" style={{width: 50, height: 50, background: 'red', display: this.state.showDiv, outline: 'none'}} onBlur={e => this.hide()} ref={this.divRef}></div>
+                <button onClick={() => this.props.createAlertMessage('test')}>click</button>
+
             </div>
         )
     }
--- src/components/UserAlert/UserAlert.js
+++ src/components/UserAlert/UserAlert.js
@@ -6,28 +6,52 @@ import { connect } from 'react-redux';
 
 class UserAlert extends Component {
     state = {
-        // timeoutTracker: {},
+        timeoutTracker: {},
         alertMessages: []
     }
     componentDidUpdate(prevProps, prevState) {
         if (prevProps.alertMessage.alertCount !== this.props.alertMessage.alertCount) {
             let alertMessages = [...this.state.alertMessages];
             alertMessages.push(this.props.alertMessage);
-            // let timeoutTracker = {...this.state.timeoutTracker}
-            // const timeoutId = this.props.alertMessage.message + this.props.alertMessage.alertCount;
-            // const alertTimeout = setTimeout(() => this.clearAlertMessage(), 4000);
-            setTimeout(() => this.clearAlertMessage(), 4000);
-            // timeoutTracker[timeoutId] = alertTimeout;
-            this.setState({alertMessages});
+            let timeoutTracker = {...this.state.timeoutTracker}
+            const timeoutId = this.props.alertMessage.message + this.props.alertMessage.alertCount;
+            const alertTimeout = setTimeout(() => this.clearAlertMessage(timeoutId), 4000);
+            timeoutTracker[timeoutId] = alertTimeout;
+            this.setState({alertMessages, timeoutTracker});
         }
     }
-    clearAlertMessage() {
+    clearAlertMessage(timeoutId) {
         let alertMessages = [...this.state.alertMessages];
-        alertMessages = alertMessages.slice(1);
+        alertMessages = alertMessages.filter(message => {
+            const messageAndCount = message.message + message.alertCount
+            return  messageAndCount !== timeoutId;
+        });
         this.setState({alertMessages})
     }
+    cancelTimeout(timeoutId) {
+        let timeoutTracker = {...this.state.timeoutTracker};
+        clearTimeout(timeoutTracker[timeoutId]);
+        delete timeoutTracker[timeoutId];
+        this.setState({timeoutTracker})
+    }
     renderAlertMessages() {
-        return this.state.alertMessages.map((alertMessage, i) => <div className="alert" key={alertMessage.message + alertMessage.alertCount}>{alertMessage.message}</div>)
+        return this.state.alertMessages.map((alertMessage, i) => {
+            return (
+               <div className="alert" key={alertMessage.message + alertMessage.alertCount}>
+                    <i className="close-message fa fa-window-close" aria-hidden="true" onClick={e => this.clearAlertMessage(alertMessage.message + alertMessage.alertCount)}></i>
+                    <i className="pin-message fas fa-thumbtack" onClick={e => this.cancelTimeout(alertMessage.message + alertMessage.alertCount)}></i>
+                    <div className="alert-text">{alertMessage.message}</div>
+                    <div 
+                        style={{
+                            animationName: 'alert-timer-animation', 
+                            animationDuration: this.state.timeoutTracker[alertMessage.message + alertMessage.alertCount] ? '3.9s' : '0s'
+                        }} 
+                        className="alert-timer">
+                    </div>
+                </div>
+            )
+        
+        })
     }
     render() {
         return (
